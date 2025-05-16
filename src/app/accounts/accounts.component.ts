2d18473efaa04faeadb48d6a2114a3320a16bd28
ZAKARIA <elguazzarzakaria10@gmail.com>
2025-05-16 19:49:06 +0100

    Some modifications

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,28 +1,39 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 import {AccountsService} from '../services/Acc/accounts.service';
+import {Account} from '../Model/model';
+import {catchError, Observable, throwError} from 'rxjs';
 
 @Component({
   selector: 'app-accounts',
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
 export class AccountsComponent implements OnInit{
-  private accounts : any;
+  private accounts ! : Observable<Array<Account>>;
+  private messageError !: Object;
   constructor(private accService:AccountsService) {
   }
 
   ngOnInit(): void {
-        this.accService.getAccounts().subscribe(
+        /*this.accService.getAccounts().subscribe(
           {
             next : (data) => this.accounts = data,
             error : (err)=> console.log(err)
           }
-        )
+        )*/
+    this.accounts=this.accService.getAccounts().pipe(
+      catchError((err)=>{
+        this.messageError=err;
+        return throwError(()=>err);
+      })
+    );
+
   }
   getAccounts(){
     return this.accounts;
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,29 +1,52 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf, NgIf} from '@angular/common';
+import {AsyncPipe,NgForOf, NgIf} from '@angular/common';
 import {CustomersService} from '../services/Cust/customers.service';
+import {catchError, Observable, throwError} from 'rxjs';
+import {Customer} from '../Model/model';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-customers',
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    //JsonPipe,
+    AsyncPipe,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css',
 })
 export class CustomersComponent implements OnInit{
-  private customers : any;
+  private customers !: Observable<Array<Customer>>;
+  // private messageError : String | undefined;
+  //private messageError : String | null=null;
+  private messageError !: Object;
+  search_customer: any;
   constructor(private custService : CustomersService) {
     }
     ngOnInit(): void {
-       this.custService.getCustomers().subscribe({
+       /*this.custService.getCustomers().subscribe({
          next : (data) => this.customers = data,
-        error : (err)=> console.log(err)
-      });
+        error : (err)=> {
+           this.messageError = err.message;
+           console.log(err);
+        }
+      });*/
+      this.customers=this.custService.getCustomers().pipe(
+        catchError(err => {
+          this.messageError=err.message;
+          return throwError(() => err);
+        })
+      );
   }
 
   public getCustomers(){
     return this.customers;
+  }
+  public getMessageError(){
+    return this.messageError;
   }
 }
 

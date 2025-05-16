9619216cb49d42729618fe8e129cf0e61ae5d2f3
ZAKARIA <elguazzarzakaria10@gmail.com>
2025-05-16 19:47:31 +0100

    Indicating the type of return of getAccounts

--- src/app/services/Acc/accounts.service.ts
+++ src/app/services/Acc/accounts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Account} from '../../Model/model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AccountsService {
   constructor(private http:HttpClient) {
 
   }
-  public getAccounts() : Observable<Object>{
-    return this.http.get("http://localhost:8080/bank_accounts/list_bank_accounts");
+  public getAccounts() : Observable<Array<Account>>{
+    return this.http.get<Array<Account>>("http://localhost:8080/bank_accounts/list_bank_accounts");
   }
 }

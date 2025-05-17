6cfc0ef11e6f96b27d26f4f64a7599319af755db
ZAKARIA <elguazzarzakaria10@gmail.com>
2025-05-17 20:25:04 +0100

    Some modifications in accounts.service.ts

--- src/app/services/Acc/accounts.service.ts
+++ src/app/services/Acc/accounts.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Account} from '../../Model/model';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
-
   constructor(private http:HttpClient) {
 
   }
   public getAccounts() : Observable<Array<Account>>{
-    return this.http.get<Array<Account>>("http://localhost:8080/bank_accounts/list_bank_accounts");
+    return this.http.get<Array<Account>>(environment.apiUrlAccounts+"/list_bank_accounts");
   }
 }

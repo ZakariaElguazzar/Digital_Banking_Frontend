aa03c2eeb571de1d93b1bebf4dc29015d452834e
ZAKARIA <elguazzarzakaria10@gmail.com>
2025-05-18 01:17:13 +0100

    Some modifications

--- src/app/interceptors/app-http.interceptor.ts
+++ src/app/interceptors/app-http.interceptor.ts
@@ -1,5 +1,18 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpInterceptorFn} from '@angular/common/http';
+import {LoginService} from '../services/Login/login.service';
+import {inject} from '@angular/core';
 
 export const appHttpInterceptor: HttpInterceptorFn = (req, next) => {
+  const logService = inject(LoginService);
+  const token = logService.accessToken
+  if (!req.url.includes(("/auth/login"))){
+    console.log(token)
+    const logReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(logReq);
+  }
   return next(req);
 };
